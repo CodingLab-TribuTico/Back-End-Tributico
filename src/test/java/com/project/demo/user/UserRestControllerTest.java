52e6c32fa368e4b39d0f37f7a5f41fdbb0360046
diegonunez19 <tu_correo_de_diegonunez19@gmail.com>
2025-08-02 01:56:57 -0600

    Add new changes

--- src/test/java/com/project/demo/user/UserRestControllerTest.java
+++ src/test/java/com/project/demo/user/UserRestControllerTest.java
@@ -1,6 +1,5 @@
 package com.project.demo.user;
 
-import com.project.demo.logic.entity.http.HttpResponse;
 import com.project.demo.logic.entity.rol.Role;
 import com.project.demo.logic.entity.rol.RoleEnum;
 import com.project.demo.logic.entity.rol.RoleRepository;
@@ -8,7 +7,6 @@ import com.project.demo.logic.entity.user.User;
 import com.project.demo.logic.entity.user.UserRepository;
 import com.project.demo.rest.user.UserRestController;
 import jakarta.servlet.http.HttpServletRequest;
-import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,16 +18,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDate;
 import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -38,7 +34,6 @@ import static org.mockito.Mockito.*;
 public class UserRestControllerTest {
 
     private static final Logger logger = LoggerFactory.getLogger(UserRestControllerTest.class);
-
 
     @Mock
     private UserRepository userRepository;
@@ -87,20 +82,28 @@ public class UserRestControllerTest {
 
         ResponseEntity<?> response = userRestController.addUser(testUser, httpServletRequest);
 
-        logger.info(() -> "Status Code: " + response.getStatusCode());
-        logger.info(() -> "Cuerpo de la respuesta: " + response.getBody());
+        logger.info(() -> "Datos de salida: \n" +
+                "Código de estado: " + response.getStatusCode());
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         verify(userRepository).save(any(User.class));
 
-
-
     }
 
+    @Test
+    @DisplayName("Debe devolver una lista paginada con los usuarios")
+    void getAllUsersTest() {
+        Page<User> usersPage = new PageImpl<>(Collections.singletonList(testUser));
+        when(userRepository.findAll(any(Pageable.class))).thenReturn(usersPage);
+        when(httpServletRequest.getRequestURL()).thenReturn(new StringBuffer("http://localhost"));
+        when(httpServletRequest.getMethod()).thenReturn("GET");
 
+        ResponseEntity<?> response = userRestController.getAll(1, 5, "", httpServletRequest);
 
+        logger.info(() -> "Datos de salida:\nStatus: " + response.getStatusCode());
 
-
-
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        verify(userRepository).findAll(any(Pageable.class));
+    }
 
 }
