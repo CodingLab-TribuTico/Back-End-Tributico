e0e744ccebee21dbf73d7326a8490d3715265fa4
diegonunez19 <tu_correo_de_diegonunez19@gmail.com>
2025-08-04 00:53:10 -0600

    Add save and export simulations

---
+++ src/main/java/com/project/demo/rest/SimulationExport/SimulationExportController.java
@@ -0,0 +1,87 @@
+package com.project.demo.rest.SimulationExport;
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.project.demo.logic.entity.isrSimulation.IsrRepository;
+import com.project.demo.logic.entity.isrSimulation.IsrSimulation;
+import com.project.demo.logic.entity.isrSimulation.SimulationExportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/export")
+public class SimulationExportController {
+
+    @Autowired
+    IsrRepository isrRepository;
+
+    @Autowired
+    SimulationExportService simulationExportService;
+
+    @GetMapping("/generate-pdf/{id}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'USER')")
+    public ResponseEntity<byte[]> generatePdf(@PathVariable Long id) {
+        Optional<IsrSimulation> foundSimulation = isrRepository.findById(id);
+
+        if (foundSimulation.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        IsrSimulation simulation = foundSimulation.get();
+
+        try {
+            String htmlBody = simulationExportService.generateSimulationPdf(simulation);
+            String formattedHtmlContent = simulationExportService.generateHtmlContent(htmlBody);
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            PdfWriter pdfWriter = new PdfWriter(outputStream);
+            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+            ConverterProperties converterProperties = new ConverterProperties();
+            HtmlConverter.convertToPdf(formattedHtmlContent, pdfDocument, converterProperties);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentDisposition(ContentDisposition.builder("attachment")
+                    .filename("simulación.pdf")
+                    .build());
+            headers.setContentType(MediaType.APPLICATION_PDF);
+
+            return new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/generate-csv/{id}")
+    public ResponseEntity<byte[]> generateCsv(@PathVariable Long id) {
+        Optional<IsrSimulation> foundSimulation = isrRepository.findById(id);
+
+        if (foundSimulation.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        IsrSimulation sim = foundSimulation.get();
+        String csvContent = simulationExportService.generateSimulationCsv(sim);
+        byte[] csvBytes = csvContent.getBytes(StandardCharsets.UTF_8);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("text/csv"));
+        headers.setContentDisposition(ContentDisposition.builder("attachment")
+                .filename("Simulación.csv")
+                .build());
+
+        return new ResponseEntity<>(csvBytes, headers, HttpStatus.OK);
+    }
+}
+
+
--- src/main/java/com/project/demo/rest/ivaCalculation/IvaCalculationController.java
+++ src/main/java/com/project/demo/rest/ivaCalculation/IvaCalculationController.java
@@ -1,13 +1,24 @@
 package com.project.demo.rest.ivaCalculation;
 
 import com.project.demo.logic.entity.http.GlobalResponseHandler;
+import com.project.demo.logic.entity.http.Meta;
+import com.project.demo.logic.entity.isrSimulation.IsrSimulation;
 import com.project.demo.logic.entity.ivacalculation.IvaCalculation;
+import com.project.demo.logic.entity.ivacalculation.IvaCalculationRepository;
+import com.project.demo.logic.entity.user.User;
+import com.project.demo.logic.entity.user.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/iva-simulation")
@@ -15,7 +26,13 @@ public class IvaCalculationController {
     @Autowired
     IvaCalculationService ivaService;
 
-    @GetMapping
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    IvaCalculationRepository ivaCalculationRepository;
+
+    @GetMapping("/create")
     @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'USER')")
     public ResponseEntity<?> createIvaSimulation(
             @RequestParam int year,
@@ -32,4 +49,54 @@ public class IvaCalculationController {
         );
 
     }
+
+    @PostMapping
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'USER')")
+    public ResponseEntity<?> saveSimulation(@RequestBody IvaCalculation simulation,
+                                            HttpServletRequest request) {
+        Optional<User> foundUser = userRepository.findById(simulation.getUser().getId());
+        if (foundUser.isPresent()) {
+            simulation.setUser(foundUser.get());
+            IvaCalculation savedSimulation = ivaCalculationRepository.save(simulation);
+            return new GlobalResponseHandler().handleResponse("Simulación guardada exitosamente",
+                    savedSimulation, HttpStatus.CREATED, request);
+
+        } else {
+            return new GlobalResponseHandler().handleResponse("No se encontro el usuario",
+                    HttpStatus.NOT_FOUND, request);
+        }
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN', 'USER')")
+    public ResponseEntity<?> getAll(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "") String search,
+            @AuthenticationPrincipal User userPrincipal,
+            HttpServletRequest request) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<IvaCalculation> simulationPage;
+
+        if (search == null || search.trim().isEmpty()) {
+            simulationPage = ivaCalculationRepository.findByUserId(userPrincipal.getId(), pageable);
+        } else {
+            simulationPage = ivaCalculationRepository.searchIvaSimulation(search.trim(), userPrincipal.getId(),
+                    pageable);
+        }
+
+        Meta meta = new Meta(request.getMethod(), request.getRequestURL().toString());
+        meta.setTotalPages(simulationPage.getTotalPages());
+        meta.setTotalElements(simulationPage.getTotalElements());
+        meta.setPageNumber(simulationPage.getNumber() + 1);
+        meta.setPageSize(simulationPage.getSize());
+
+        return new GlobalResponseHandler().handleResponse(
+                "Simulaciones recuperadas exitosamente",
+                simulationPage.getContent(),
+                HttpStatus.OK,
+                meta);
+    }
+
+
 }
