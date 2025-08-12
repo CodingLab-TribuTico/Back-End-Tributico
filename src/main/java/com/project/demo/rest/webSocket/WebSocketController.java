7a3ed33b444ccb6b0ccc04cdf2725bbbb45589f0
Jgonzalez-tech <jgonzalezp@ucenfotec.ac.cr>
2025-08-11 23:10:22 -0600

    Minor fix notificationController

--- src/main/java/com/project/demo/rest/webSocket/WebSocketController.java
+++ src/main/java/com/project/demo/rest/webSocket/WebSocketController.java
@@ -1,17 +1,10 @@
 package com.project.demo.rest.webSocket;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Controller
 public class WebSocketController {
@@ -19,7 +12,6 @@ public class WebSocketController {
     @MessageMapping("/test")
     @SendTo("/topic/test-responses")
     public String handleTestMessage(@Payload String message) {
-        System.out.println("Mensaje recibido del cliente: " + message);
         return "Respuesta del servidor: " + message.toUpperCase();
     }
 }
